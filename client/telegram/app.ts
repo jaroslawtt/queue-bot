7a4c7b77b10af6d7b3aaee0e411ef12f75ee3855
jaroslawtt <yaroslavlogan61907@gmail.com>
2022-11-17 16:51:42 +0200

    Added .end module

--- client/telegram/app.ts
+++ client/telegram/app.ts
@@ -1,5 +1,18 @@
-console.log(`Hello, world`);
+import { config } from "dotenv";
+import TelegramBot from "node-telegram-bot-api";
 
-console.log(`hello, blyat`)
+config({path: `.env`});
 
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN as string,{
+    polling:{
+        interval:300,
+        autoStart: true,
+        params:{
+            timeout:10,
+        }
+    },
+});
 
+bot.onText(/\/start/, async (msg) => {
+    await bot.sendMessage(msg.chat.id, `Hello`);
+});
